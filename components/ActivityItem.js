1fc159039e23dade2fc075615fdb545ceb6f1b41
et <eric.thomson13@gmail.com>
2020-02-27 15:34:31 -0700

    Resolved Require Loop

--- components/ActivityItem.js
+++ components/ActivityItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import ActivityList from './ActivityList';
 
 const ActivityItem = ({ id, name, summary, difficulty, stars, location, url, length, latitude, longitude, }) => {
   return (
