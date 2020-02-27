63688988e66e5b00896b6465bfa929c76bfc63a4
et <eric.thomson13@gmail.com>
2020-02-26 19:54:24 -0700

    Folder and Component Layout Started

---
+++ screens/WelcomeView.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+const WelcomeView = (props) => {
+
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default WelcomeView;
---
+++ utility/Button.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const Button = (props) => {
+
+};
+
+const styles = StyleSheet.create({ 
+
+});
+
+export default Button;
