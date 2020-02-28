9d1b84369edb37f41c76f677999f4d68c0f12719
ericthomson13 <57376746+ericthomson13@users.noreply.github.com>
2020-02-27 18:57:42 -0700

    Delete Keys.js

--- Keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  darksky: '7a702fea4cdfed3d58339208df6a99de',
-  googleMaps: 'AIzaSyATf169ztDvZS-yjjQ5JcxLhZzN0yRtZCo',
-  mountainProject: '106214457-750bdcc7e64a18f8123021b00622aca7',
-  hikingProject: '106214457-f107479598439c0b476f5fd6d68d06d5',
-
-}
