15dc978825cbb49985afae2a490290ae6a841af8
et <eric.thomson13@gmail.com>
2020-03-02 15:24:11 -0700

    Option Button Updated

--- components/utility/OptionButton.js
+++ components/utility/OptionButton.js
@@ -5,23 +5,34 @@ const OptionButton = ({ name, setSelectedActivity }) => {
   return (
     <View>
       <TouchableOpacity onPress={() => setSelectedActivity(name)} style={styles.button} >
-        <Text>
+        <Text style={styles.text} >
           {name}
         </Text>
       </TouchableOpacity>
     </View>
-    
   );
 };
 
 const styles = StyleSheet.create({ 
   buttonView: {
-    alignContent: 'center',
-    justifyContent: 'center',
+    alignItems: 'center',
+    justifyItems: 'center',
   },
   // this color isn't working yet
   button: {
     borderColor: '#BFD1E5',
+    backgroundColor: '#126659',
+    alignItems: 'center',
+    justifyItems: 'center',
+    borderRadius: 10,
+
+  },
+  text: {
+    borderColor: '#BFD1E5',
+    color: '#FFF275',
+    fontSize: 18,
+    padding: 5,
+    margin: 5,
   }
 });
 
