e0c61b5f0ad2a4f4cecc13db76329ac05c931e69
et <eric.thomson13@gmail.com>
2020-03-02 17:34:45 -0700

    Web-Maps Module Workaround

--- components/maps/MapWindow.js
+++ components/maps/MapWindow.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet, } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, Platform, } from 'react-native';
 import { useSelector, } from 'react-redux';
+
+// this allows to not break in web because react-native-web-maps not working well 2MAR2020
+  
+let MapView = null;
+Platform.OS === 'web' ? null :  MapView = require('react-native-maps');
 
 const MapWindow = () => {
   const trails = useSelector((state) => state.activity.activityItems);
   const selectedIndex = useSelector((state) => state.activity.mapView);
   const selected = trails[selectedIndex];
 
-  return (
-    <View />
-  );
-};
+  if (MapView !== null) {
+    return (
+      <View>
+        <MapView style={styles.map} />
+      </View>
+    );
+  }
+  return null;  
+}
 
 const styles = StyleSheet.create({
 
--- screens/MapViewScreen.js
+++ screens/MapViewScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, } from 'react-native';
 import { useDispatch, } from 'react-redux';
 
 import MapWindow from '../components/maps/MapWindow';
