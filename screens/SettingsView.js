6a7be33cad3d0b5d77831779f4f0c4871246ea7a
et <eric.thomson13@gmail.com>
2020-03-04 16:46:25 -0700

    Location Toggling With Default

--- screens/SettingsView.js
+++ screens/SettingsView.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { View, Text, StyleSheet, } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, } from 'react-redux';
+import AsyncStorage from '@react-native-community/async-storage';
+import DropdownMenu from 'react-native-dropdown-menu';
 
 import HomeButton from '../components/utility/HomeButton';
 
-// TODO: make this functional and add to API calls
-
 const SettingsView = () => {
-  const units = useSelector((state) => state.settings.units);
+  const dispatch = useDispatch();
+
+  const _storeSettings = async (target, setting) => {
+    try {
+      await AsyncStorage.setItem(`@whether_trails_${target}`,  setting);
+    } catch (error) {
+      console.log('error in storing settings ', error);
+    }
+  };
+
   return (
     <View>
       <View style={styles.container} >
         <Text style={styles.heading} >
           Settings:
         </Text>
+        <View>
+          <View style={styles.unitsView}>
+            <Text style={styles.unitsTitle}>Units</Text>
+            <DropdownMenu 
+              data={['imperial', 'metric']} 
+              handler={(selection, row) => { console.log('selection: ', selection, ' row: ', row);}}
+            />
+          </View>
+        </View>
       </View>
       <HomeButton />
     </View>
--- store/actions/settingsActions.js
+++ store/actions/settingsActions.js
@@ -1,5 +1,6 @@
 export const SET_UNITS = 'SET_UNITS';
 export const SET_RANGE = 'SET_RANGE';
+export const SET_NUM_TRAILS = 'SET_NUM_TRAILS';
 
 export const setUnits = (units) => {
   return ({
